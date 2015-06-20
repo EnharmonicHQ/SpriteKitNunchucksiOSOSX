edb512e4da786328589a105be2a7392f64dffe6e
Jonathan Saggau <jsaggau@enharmonichq.com>
2015-06-20 04:06:48 -0400

    Testing how many chucks we could draw this way were it not for physics...

--- SpriteKit0/ENHOSXScene.m
+++ SpriteKit0/ENHOSXScene.m
@@ -19,21 +19,7 @@
 {
     if (![self.mouseNode parent])
     {
-        //Make a new chuck if the mouse wasn't dragging around (mouse node has no parent)
-        CGPoint location = [theEvent locationInNode:self];
-        SKNode *spriteOne = [self makeNunchuckAtLocation:location withBackgroundColor:[SKColor greenColor] withStrokeColor:[SKColor blackColor]];
-        location.x += spriteOne.frame.size.width;
-        SKNode *spriteTwo = [self makeNunchuckAtLocation:location withBackgroundColor:[SKColor redColor] withStrokeColor:[SKColor blackColor]];
-        [self addChild:spriteOne];
-        [self addChild:spriteTwo];
-        location.x -= spriteOne.frame.size.width / 2.f;
 
-        SKPhysicsJointPin *chuckJoint = [SKPhysicsJointPin jointWithBodyA:spriteOne.physicsBody bodyB:spriteTwo.physicsBody anchor:location];
-
-        chuckJoint.frictionTorque = 0.2;
-        SKPhysicsWorld *world = [self physicsWorld];
-        [world addJoint:chuckJoint];
-        [self wiggleStuffWithMagnitude:40.0f];
     }
     else
     {
