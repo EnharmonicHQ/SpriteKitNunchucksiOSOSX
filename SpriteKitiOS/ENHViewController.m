04f6a70403832fdf05df290c372864fe6d3ebada
Jonathan Saggau <jsaggau@enharmonichq.com>
2015-10-10 20:48:39 -0400

    iOS 9 modernization: 	- Use a storyboard for the launch screen. 	- Use a single storyboard for all screen sizes. 	- Use gesture recognizers on iOS.

--- SpriteKitiOS/ENHViewController.m
+++ SpriteKitiOS/ENHViewController.m
@@ -41,7 +41,7 @@
     return YES;
 }
 
-- (NSUInteger)supportedInterfaceOrientations
+- (UIInterfaceOrientationMask)supportedInterfaceOrientations
 {
     if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
         return UIInterfaceOrientationMaskAllButUpsideDown;
